d8c082e5cf73677db4690f94381a57b5771ee0c9
Sofia Kondirova <Sofia.Kondirova@jetbrains.com>
2025-01-05 21:45:48 +0100

    uploading deployed artifacts

---
+++ src/main/java/com/example/star_repository/HomePageController.java
@@ -0,0 +1,14 @@
+package com.example.star_repository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "index";
+    }
+}
--- src/main/java/com/example/star_repository/RepositoryController.java
+++ src/main/java/com/example/star_repository/RepositoryController.java
@@ -5,11 +5,16 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 @RestController
@@ -18,28 +23,20 @@ public class RepositoryController {
 
     private static final String REPOSITORY_PATH = "repository/";
 
-//    @GetMapping("repository/{artifactId}/{version}")
-//@GetMapping("repository/{groupId:.+}/{artifactId}/{version}")
-//@GetMapping("repository/{groupId}/{artifactId}/{version}")
+    //Download an artifact from the repository
     @GetMapping("repository/{*fullPath}")
     public ResponseEntity<Resource> downloadByGAV(
-//        @PathVariable String groupId,
-//        @PathVariable String artifactId,
-//        @PathVariable String version
-        @PathVariable String fullPath
+            @PathVariable String fullPath
 
     ) {
- //      String groupPath = groupId.replace('.', '/');
         String[] parts = fullPath.split("/");
         String groupId = String.join("/", Arrays.copyOfRange(parts, 0, parts.length - 2));
         String artifactId = parts[parts.length - 2];
         String version = parts[parts.length - 1];
 
-                String fileName = groupId + "/" + artifactId + "/" + version;
+        String fileName = groupId + "/" + artifactId + "/" + version;
 //        String fileName = groupId + "/" + artifactId + "/" + version + "/" + artifactId + "-" + version + ".jar";
         File file = new File(REPOSITORY_PATH + fileName);
-
-
 
         if (file.exists()) {
             Resource resource = new FileSystemResource(file);
@@ -50,5 +47,32 @@ public class RepositoryController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
+
+    //Load artifacts to the repository
+    @PutMapping("repository/{*path}")
+    public ResponseEntity<String> uploadFile(@PathVariable String path, InputStream inputStream) {
+        try {
+            File targetFile = new File(REPOSITORY_PATH + path);
+            File dir = targetFile.getParentFile();
+
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(targetFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            }
+
+            return ResponseEntity.ok("File uploaded successfully!");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error uploading file: " + e.getMessage());
+        }
+    }
+
 }
 
